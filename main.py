a1d007d166259c72d8ff227061ee5d1aaa245857
Kotian-99 <140294806+Kotian-99@users.noreply.github.com>
2023-11-03 17:21:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,28 +6,28 @@ def classify_battery_by_soh(present_capacity, rated_capacity=120):
         return "exchange"
     else:
         return "failed"
-      
+
 def count_batteries_by_health(present_capacities):
-  return {
-    "healthy": 0,
-    "exchange": 0,
-    "failed": 0
-  }
- for capacity in present_capacities:
+    counts = {
+        "healthy": 0,
+        "exchange": 0,
+        "failed": 0
+    }
+
+    for capacity in present_capacities:
         soh_category = classify_battery_by_soh(capacity)
         counts[soh_category] += 1
 
     return counts
 
 def test_bucketing_by_health():
-  print("Counting batteries by SoH...\n")
-  present_capacities = [113, 116, 80, 95, 92, 70]
-  counts = count_batteries_by_health(present_capacities)
-  assert(counts["healthy"] == 2)
-  assert(counts["exchange"] == 3)
-  assert(counts["failed"] == 1)
-  print("Done counting :)")
-
+    print("Counting batteries by SoH...\n")
+    present_capacities = [113, 116, 80, 95, 92, 70, 120, 60]
+    counts = count_batteries_by_health(present_capacities)
+    assert(counts["healthy"] == 4)
+    assert(counts["exchange"] == 3)
+    assert(counts["failed"] == 2)
+    print("Done counting :)")
 
 if __name__ == '__main__':
-  test_bucketing_by_health()
+    test_bucketing_by_health()

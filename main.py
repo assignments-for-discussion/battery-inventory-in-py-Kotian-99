9d97bfc11c840bf9df161cdf6d7a625732f732a2
Kotian-99 <140294806+Kotian-99@users.noreply.github.com>
2023-11-03 17:16:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-
+def classify_battery_by_soh(present_capacity, rated_capacity=120):
+    soh = (present_capacity / rated_capacity) * 100
+    if soh > 80:
+        return "healthy"
+    elif 62 <= soh <= 80:
+        return "exchange"
+    else:
+        return "failed"
+      
 def count_batteries_by_health(present_capacities):
   return {
     "healthy": 0,
     "exchange": 0,
     "failed": 0
   }
+ for capacity in present_capacities:
+        soh_category = classify_battery_by_soh(capacity)
+        counts[soh_category] += 1
 
+    return counts
 
 def test_bucketing_by_health():
   print("Counting batteries by SoH...\n")
